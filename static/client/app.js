7847b4a66bc97f23fc65f194c2290e31e8598953
Iliyan <iliyan.ivanov@zautre.com>
2022-10-17 15:40:19 +0300

    Adding functionality to show only available vans

--- static/client/app.js
+++ static/client/app.js
@@ -1,6 +1,8 @@
 initAnimations();
 initBookingForm();
 
+const googleApiKey = 'AIzaSyDEyI-WDW_xvepethIYS3XA11r73NgaVRc';
+
 function initBookingForm() {
     // init datepicker events
     $(function () {
@@ -28,6 +30,49 @@ function initBookingForm() {
     });
 }
 
+// todo write actual calIDs
+let vans = {
+    roscinante: 'c_87a464ad7365e9c770b8325b7d8fc7604ecaaa59e9a5e55b4a59fa2f99aedcbe@group.calendar.google.com',
+    alfonso: 'c_ac2c84e6fb497a217cd923b1431ce89a099db8a6522e45731e08edeccee7e3ab@group.calendar.google.com',
+    bucephalus: 'c_1637599e2a7e1a46106c3fe3bb1065ffe0fbe98d1f30d69452983ab790ac6eda@group.calendar.google.com'
+}
+
+/**
+ * Gets calender events between dates for given calender/van
+ */
+function getCalenderEvents(calendarID, startDate, endDate) {
+    let parameters = "timeMin=" + encodeURIComponent(startDate.format()) + "&timeMax=" + encodeURIComponent(endDate.format())
+
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: "https://www.googleapis.com/calendar/v3/calendars/" + calendarID + "/events?" + parameters + "&key=" + googleApiKey,
+            success: function (data) {
+                resolve(data)
+            },
+            error: (data) => {
+                reject(data)
+            }
+        });
+    })
+}
+
+function showAvailableVans(startDate, endDate) {
+    Object.keys(vans).forEach((van) => {
+        getCalenderEvents(vans[van], startDate, endDate)
+            .then((data) => {
+                if (data.items.length === 0) {
+                    $('.van[data-name="' + data.summary.toLowerCase() + '"]').css('display', 'block')
+                }
+                console.log('Promise succ response: ')
+                console.log(data)
+            })
+            .catch((data) => {
+                console.log('Promise error for: ')
+                console.log(data)
+            });
+    })
+}
+
 async function initSecondBookingStep(startDate, endDate) {
     $('html, body').animate({
         scrollTop: $("#booking-step-2").offset().top - 150
@@ -37,9 +82,7 @@ async function initSecondBookingStep(startDate, endDate) {
         $('.booking-step-2').addClass('active');
     }
 
-    let availability = await getVanAvailability(startDate, endDate);
-
-    showAvailableVans(availability);
+    showAvailableVans(startDate, endDate);
 
     $('.van').on('click', (e) => {
         let $target = $(e.target);
@@ -57,22 +100,6 @@ async function initSecondBookingStep(startDate, endDate) {
 
 function hideSecondBookingStep() {
     $('.booking-step-2').removeClass('active');
-}
-
-function getVanAvailability() {
-    return [
-        { santimento: true },
-        { erectino: true },
-        { ivan: false }
-    ]
-}
-
-function showAvailableVans(availability) {
-    availability.forEach((van) => {
-        if (Object.values(van)[0]) {
-            $('.van[data-name="' + Object.keys(van)[0] + '"]').css('display', 'block')
-        }
-    })
 }
 
 function initThirdBookingStep(vanName, dateRange) {
@@ -98,5 +125,3 @@ function initThirdBookingStep(vanName, dateRange) {
 function hideThirdBookingStep() {
     $('.booking-step-3').removeClass('active');
 }
-
-// clientID: 32269105429-9lc8egbuervued8vqstk4f0j7a1eugcp.apps.googleusercontent.com
